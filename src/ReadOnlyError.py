558606dff5e923877a7dfb27cdcf31c98e1f5e65
Ghostkeeper <rubend@tutanota.com>
2018-01-25 16:18:42 +0100

    Add exception for when something is written to that was read-only
    This allows us to distinguish between this and normal permission errors raised by the system.
    
    Contributes to issue CURA-4872.

---
+++ src/ReadOnlyError.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2018 Ultimaker B.V.
+# libCharon is released under the terms of the LGPLv3 or higher.
+
+##  Exception to indicate that an attempt was made to write to a resource that
+#   is read-only.
+#
+#   Normally this sort of thing would be a ``PermissionError`` (the built-in
+#   Python exception), but we want to be able to distinguish between these
+#   errors ordinary ``PermissionErrors`` raised by the file system not having
+#   access to that file.
+class ReadOnlyError(PermissionError):
+    ##  Creates the exception instance.
+    #   \param virtual_path The resource that could not be written to. If not
+    #   provided, an empty string is used which indicates that the entire file
+    #   could not be written to.
+    def __init__(self, virtual_path: str = ""):
+        self.virtual_path = virtual_path
+
+    ##  Provides a human-readable version of this error for in the stack trace.
+    def __repr__(self):
+        return "ReadOnlyError({resource})".format(resource = self.virtual_path)
