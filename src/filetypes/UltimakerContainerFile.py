9d42a5cca0d05ddbc0c56081176aaafc333fdc27
Ghostkeeper <rubend@tutanota.com>
2018-01-26 09:49:04 +0100

    Add empty rels file
    Whenever we have a file with relations we should add to this element and rewrite the rels file.
    
    Contributes to issue CURA-4872.

--- src/filetypes/UltimakerContainerFile.py
+++ src/filetypes/UltimakerContainerFile.py
@@ -2,6 +2,7 @@
 # libCharon is released under the terms of the LGPLv3 or higher.
 
 from typing import Optional
+import xml.etree.ElementTree as ET #For writing XML manifest files.
 import zipfile
 
 from ..FileInterface import FileInterface #The interface we're implementing.
@@ -10,9 +11,15 @@ from ..OpenMode import OpenMode #To detect whether we want to read and/or write 
 ##  A container file type that contains multiple 3D-printing related files that
 #   belong together.
 class UltimakerContainerFile(FileInterface):
+    xml_header = ET.ProcessingInstruction("xml", "version=\"1.0\" encoding=\"UTF-8\"")
+
     def open(self, path: Optional[str] = None, mode: OpenMode = OpenMode.ReadOnly):
         self.mode = mode
         self.zipfile = zipfile.ZipFile(path, self.mode.value, compression = zipfile.ZIP_DEFLATED)
+        self.relations_element = ET.Element("Relationships", xmlns = "http://schemas.openxmlformats.org/package/2006/relationships")
+
+        #Set up an empty container.
+        self.zipfile.writestr("_rels/.rels", ET.tostring(self.xml_header) + "\n" + ET.tostring(self.relations_element))
 
     def close(self):
         self.flush()
