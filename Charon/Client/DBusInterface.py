d1e2d435a893b1f21e3d01f4328d7dacbce9c15f
Arjen Hiemstra <ahiemstra@heimr.nl>
2018-02-01 13:03:49 +0100

    Implement most of DBusInterface
    For both dbus-python and QtDBus

--- Charon/Client/DBusInterface.py
+++ Charon/Client/DBusInterface.py
@@ -3,8 +3,8 @@ from typing import Callable
 
 _has_qt = False
 try:
-    from PyQt5.QtCore import QCoreApplication
-    from PyQt5.QtDBus import QDBusConnection
+    from PyQt5.QtCore import QCoreApplication, QObject, pyqtSlot
+    from PyQt5.QtDBus import QDBusConnection, QDBusMessage, QDBusReply, QDBusInterface
     _has_qt = True
 except ImportError:
     pass
@@ -26,19 +26,26 @@ class DBusInterface:
         cls.__ensureDBusSetup()
 
         if cls.__use_qt:
-            pass
+            message = QDBusMessage.createMethodCall(service_path, object_path, interface, method_name)
+            message.setArguments(args)
+            result = QDBusReply(cls.__connection.call(message))
+            if result.isValid():
+                return result.value()
+            else:
+                return 0
+
         else:
-            cls.__connection.call_blocking(service_path, object_path, interface, method_name, signature, args)
+            return cls.__connection.call_blocking(service_path, object_path, interface, method_name, signature, args)
 
     @classmethod
     def connectSignal(cls, signal_name: str, callback: Callable[..., None], *, service_path: str = DefaultServicePath, object_path: str = DefaultObjectPath, interface: str = DefaultInterface) -> bool:
         cls.__ensureDBusSetup()
 
         if cls.__use_qt:
-            #self.__dbus_bus.
-            pass
+            return cls.__signal_forwarder.addConnection(service_path, object_path, interface, signal_name, callback)
         else:
             cls.__connection.add_signal_receiver(callback, signal_name, interface, service_path, object_path)
+            return True
 
     @classmethod
     def disconnectSignal(cls):
@@ -46,17 +53,59 @@ class DBusInterface:
 
     @classmethod
     def __ensureDBusSetup(cls):
+        if cls.__connection:
+            return
+
         if _has_qt and QCoreApplication.instance():
             if os.environ.get("CHARON_USE_SESSION_BUS", 1) == 1:
                 cls.__connection = QDBusConnection.sessionBus()
             else:
                 cls.__connection = QDBusConnection.systemBus()
+
+            cls.conn = cls.__connection
+            cls.__signal_forwarder = DBusSignalForwarder(cls.__connection)
+            cls.__use_qt = True
             return
 
         if os.environ.get("CHARON_USE_SESSION_BUS", 1) == 1:
-            cls.__connection = dbus.SessionBus(private=True, mainloop=dbus.mainloop.glib.DBusGMainLoop())
+            cls.__connection = dbus.Bus.get_session()
         else:
-            cls.__connection = dbus.SystemBus(private=True, mainloop=dbus.mainloop.glib.DBusGMainLoop())
+            cls.__connection = dbus.Bus.get_system()
 
     __use_qt = False
     __connection = None
+    conn = None
+    __signal_forwarder = None
+
+if _has_qt:
+    class DBusSignalForwarder(QObject):
+        def __init__(self, dbus_connection):
+            super().__init__()
+            self.__connection = dbus_connection
+            self.__connection.registerObject("/" + str(id(self)), self)
+
+            self.__interface_objects = {}
+            self.__connected_signals = set()
+            self.__callbacks = {}
+
+        def addConnection(self, service_path, object_path, interface, signal_name,  callback):
+            connection = (object_path, interface, signal_name)
+            if connection not in self.__connected_signals:
+                self.__connection.connect(service_path, object_path, interface, signal_name, self.handleSignal)
+                self.__connected_signals.add(connection)
+
+            if connection not in self.__callbacks:
+                self.__callbacks[connection] = []
+            self.__callbacks[connection].append(callback)
+
+        def removeConnection(self, service_path, object_path, interface, signal_name, callback):
+            pass
+
+        @pyqtSlot(QDBusMessage)
+        def handleSignal(self, message):
+            connection = (message.path(), message.interface(), message.member())
+            if connection not in self.__callbacks:
+                return
+
+            for callback in self.__callbacks[connection]:
+                callback(*message.arguments())
