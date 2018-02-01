af1329b73ea8ac7c10c21418c5837d6f9a0f44b5
Arjen Hiemstra <ahiemstra@heimr.nl>
2018-02-01 13:02:55 +0100

    Default to session bus for the Service as well

--- Charon/Service/main.py
+++ Charon/Service/main.py
@@ -13,7 +13,7 @@ dbus.mainloop.glib.threads_init()
 _loop = GLib.MainLoop()
 
 # Use a single bus object for all dbus communication.
-if "CHARON_USE_SESSION_BUS" in os.environ:
+if os.environ.get("CHARON_USE_SESSION_BUS", 1) == 1:
     _bus = dbus.SessionBus(private=True, mainloop=dbus.mainloop.glib.DBusGMainLoop())
 else:
     _bus = dbus.SystemBus(private=True, mainloop=dbus.mainloop.glib.DBusGMainLoop())
