4c252b052665985778185f7e9b8832f98feec313
Arjen Hiemstra <ahiemstra@heimr.nl>
2018-02-07 13:35:40 +0100

    Update cpack config so it is actually usable

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -4,20 +4,14 @@ set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Library to read and write file packages."
 set(CPACK_PACKAGE_VERSION_MAJOR 1)
 set(CPACK_PACKAGE_VERSION_MINOR 0)
 set(CPACK_PACKAGE_VERSION_PATCH 0)
-set(CPACK_GENERATOR "DEB;RPM")
-
-set(RPM_REQUIRES
-    "python3 >= 3.5.0"
-    "python3-qt5 >= 5.6.0"
-    "qt5-qtgui >= 5.6.0"
-)
-string(REPLACE ";" "," RPM_REQUIRES "${RPM_REQUIRES}")
-set(CPACK_RPM_PACKAGE_REQUIRES ${RPM_REQUIRES})
+set(CPACK_GENERATOR "DEB")
 
 set(DEB_DEPENDS
     "python3 (>= 3.5.0)"
     "python3-pyqt5 (>= 5.6.0)"
     "python3-pyqt5.qtgui (>= 5.6.0)"
+    "python3-dbus (>= 1.2.0)"
+    "dbus (>= 1.8.0)"
 )
 string(REPLACE ";" "," DEB_DEPENDS "${DEB_DEPENDS}")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS ${DEB_DEPENDS})
