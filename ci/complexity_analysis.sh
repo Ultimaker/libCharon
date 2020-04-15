0c566ffe23d702414eb810374eb80a3f8ef788fd
Bryan Foley <b.foley@ultimaker.com>
2020-04-15 14:33:44 +0200

    Added a small comment on the target value of cyclomatic complexity

--- ci/complexity_analysis.sh
+++ ci/complexity_analysis.sh
@@ -3,6 +3,6 @@
 set -eu
 
 # TODO set limits, shouldn't expect 100%
-lizard -Eduplicate Charon -T cyclomatic_complexity=20
+lizard -Eduplicate Charon -T cyclomatic_complexity=20 #This value shall not increase, target is <= 10
 
 exit 0
