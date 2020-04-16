dc0b8ae285d4d3214783d701d57019148566c7af
Bryan Foley <b.foley@ultimaker.com>
2020-04-16 12:28:46 +0200

    Reinstate setup.py so cura can install the it as a python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2018 Ultimaker B.V.	
+# libCharon is released under the terms of the LGPLv3 or higher.	
+
+from distutils.core import setup	
+
+setup(	
+    name = "Charon",	
+    version = "1.0",	
+    description = "Library to read and write file packages.",	
+    author = "Ultimaker",	
+    author_email = "r.dulek@ultimaker.com",	
+    url = "https://github.com/Ultimaker/libCharon",	
+    packages = ["Charon", "Charon.Client", "Charon.Service", "Charon.filetypes"]	
+)
