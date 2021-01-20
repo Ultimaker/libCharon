f2571807de8d49f002a52838b1c59d6368556418
Tom Clement <mr@tomclement.nl>
2021-01-20 14:17:42 +0100

    Cleaned up GCodeSocket import in VirtualFile

--- Charon/VirtualFile.py
+++ Charon/VirtualFile.py
@@ -5,10 +5,10 @@ import os
 from Charon.FileInterface import FileInterface  # The interface we're implementing.
 from Charon.OpenMode import OpenMode  #To open local files with the selected open mode.
 # The supported file types.
-from Charon.filetypes import GCodeSocket
 from Charon.filetypes.UltimakerFormatPackage import UltimakerFormatPackage
 from Charon.filetypes.GCodeFile import GCodeFile
 from Charon.filetypes.GCodeGzFile import GCodeGzFile
+from Charon.filetypes.GCodeSocket import GCodeSocket
 
 extension_to_mime = {
     ".ufp": "application/x-ufp",
