eb1de66d077c395979a30b092b66c12514160397
Marijn Deé <m.dee@ultimaker.com>
2021-06-16 14:21:04 +0200

    CES-787 Make release to buster by default

--- release.sh
+++ release.sh
@@ -9,9 +9,14 @@ set -o pipefail
 
 usage()
 {
-    echo "Usage: ${0} [OPTIONS] <release version>"
+    echo "Usage: ${0} [OPTIONS] <release version> <release distribution>"
     echo "Triggers the release of this package to the CloudSmith package storage, given the"
     echo "release version passed as argument to the script, e.g. 6.0.1 or 6.0.1-dev."
+    echo ""
+    echo "An optional release distribution on CloudSmith can be passed in after the release version argument."
+    echo "By default all packages will be released as Debian Buster compatible packages."
+    echo "Packages can be released to cloudsmith as Debian Jessie compatible by passing in the argument \"jessie\","
+    echo "e.g \"./release.sh 6.0.1 jessie\" will release a Debian Buster compatible package to the packages-official repository"
     echo ""
     echo "This script wil create a tag and push that to origin, this triggers the CI job to release"
     echo "to CloudSmith. The CI release job will differentiate between pushing to official release"
@@ -83,16 +88,29 @@ while getopts ":h" options; do
 done
 shift "$((OPTIND - 1))"
 
-if [ "${#}" -ne 1 ]; then
-    echo "Too much or too little arguments, arguments should be exactly one: <release_version>."
+#Check that the minimum amount of required arguments, at least the version number, has been provided
+if [ "${#}" -lt 1 ]; then
+    echo ""
+    echo "Too little arguments, arguments should be at least one: <release_version>."
+    echo ""
     usage
     exit 1
 fi
 
 RELEASE_VERSION="${1}"
-TAG="$(git rev-parse --abbrev-ref HEAD)-v${RELEASE_VERSION}"
+RELEASE_DISTRO="${2:-buster}"
 
-if echo "${RELEASE_VERSION}" | grep -E '^[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+(-dev)?$'; then
+TAG="$(git rev-parse --abbrev-ref HEAD)-v${RELEASE_VERSION}-${RELEASE_DISTRO}"
+
+#Check that a recognizable distribution has been provided
+if [[ ! "${RELEASE_DISTRO}" =~ ^(jessie|buster)$ ]]; then
+  echo ""
+  echo "The provided release distribution: '${RELEASE_DISTRO}', is not known. Please provide a supported distribution: 'jessie' or 'buster'"
+  echo ""
+  exit 1
+fi
+
+if echo "${RELEASE_VERSION}" | grep -E '^[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+(-dev|-[0-9]+)?$'; then
 
     if is_tag_on_github; then
         echo "Error: Cannot continue, tag is already on GitHub."
@@ -100,7 +118,7 @@ if echo "${RELEASE_VERSION}" | grep -E '^[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+(-
     fi
 
     if trigger_release; then
-        echo "Successfully triggered release '${RELEASE_VERSION}', follow the build at: http://34.90.73.76/dashboard."
+        echo "Successfully triggered release '${RELEASE_VERSION}' to distribution '${RELEASE_DISTRO}' on Cloudsmith, follow the build at: http://34.90.73.76/dashboard."
         exit 0
     fi
 
