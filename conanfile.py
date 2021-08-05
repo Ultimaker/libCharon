b2e57a984b079d76a9953925fe264c989980b1ac
Jelle Spijker <j.spijker@ultimaker.com>
2021-08-05 15:04:10 +0200

    Added Conan support
    I'm currently working on integrating Conan as a dependency manager in my workflow.
    While Charon doesn't have a lot of dependencies, it is one of Cura.
    This script allows Conan to create a package which stores the Python files and
    adds that path to the PYTHONPATH.
    
    > **Pro tip!**
    > Install Conan
    >
    > Install Cura configuration files:
    > `conan config install https://github.com/jellespijker/conan-config.git`
    >
    > If you want to let Conan handle the dependencies just use:
    > `-DCMAKE_TOOLCHAIN_FILE=cmake-build-<build_type>/conan_toolchain.cmake`
    > Install Conan plugin in Clion go to Conan settings `ctrl+alt+s`add Conan arguments
    > `--build=missing pr:b cura_release.jinja`
    > Match the build profiles with the Conan profiles in the Conan plugin sidebar
    >
    > If you want to use this as a dependency add the requirement `Charon/4.10.0@ultimaker/testing`
    > to your `conanfile.py` or `conanfile.txt`
    > If you want to use this source as the dependency (work on multiple projects at the same time)
    > make this an editable with `conan editable . Charon/4.10.0@ultimaker/testing` the auto
    > generated config files should now point to these source files and the compiled binaries.

---
+++ conanfile.py
@@ -0,0 +1,66 @@
+import os
+import pathlib
+
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+
+class CharonConan(ConanFile):
+    name = "Charon"
+    version = "4.10.0"
+    license = "LGPL-3.0"
+    author = "Ultimaker B.V."
+    url = "https://github.com/Ultimaker/libCharon"
+    description = "File metadata and streaming library"
+    topics = ("conan", "python", "cura", "ufp")
+    settings = "os", "compiler", "build_type", "arch"
+    exports = "LICENSE"
+    options = {
+        "python_version": "ANY"
+    }
+    default_options = {
+        "python_version": "3.8"
+    }
+    scm = {
+        "type": "git",
+        "subfolder": ".",
+        "url": "auto",
+        "revision": "auto"
+    }
+
+    def build_requirements(self):
+        self.build_requires("cmake/[>=3.16.2]")
+
+    def generate(self):
+        cmake = CMakeDeps(self)
+        cmake.generate()
+
+        tc = CMakeToolchain(self)
+        tc.variables["CURA_PYTHON_VERSION"] = self.options.python_version
+        tc.generate()
+
+    _cmake = None
+
+    def configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure()
+        return self._cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self.configure_cmake()
+        cmake.install()
+        self.copy("*", src = os.path.join("package", "lib", f"python{self.options.python_version}", "site-packages"), dst = "site-packages")
+
+    def package_info(self):
+        if self.in_local_cache:
+            self.runenv_info.prepend_path("PYTHONPATH", self.source_folder)
+        else:
+            self.runenv_info.prepend_path("PYTHONPATH", str(pathlib.Path(__file__).parent.absolute()))
+
+    def package_id(self):
+        self.info.header_only()
